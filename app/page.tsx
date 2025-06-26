5b83056703c9db8267d3c4b2466727be4bd91250
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2025-06-26 10:21:11 +0300

    Fix favicon issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Hero from "@/sections/Hero";
 export default function Home() {
   return (
     <div className="flex flex-col space-y-2 items-start justify-items-center sm:justify-center lg:justify-end align-bottom min-h-screen p-8 pb-20 pt-64 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-midnight text-white">
+
       <Hero />
+
     </div>
   );
 }
